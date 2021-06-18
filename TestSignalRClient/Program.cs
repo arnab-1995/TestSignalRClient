99b660dab634427ca2a8acf1cdf9289c34440a35
Arnab Mullick <Arnab.Mullick@hyland.com>
2021-06-18 09:55:07 +0530

    Add project files.

---
+++ TestSignalRClient/Program.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
+using Newtonsoft.Json.Serialization;
+using System;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace TestSignalRClient
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var hubConnection = new HubConnectionBuilder()
+                                 .WithUrl("https://localhost:5001/testhub")
+                                 .AddNewtonsoftJsonProtocol(options =>
+                                 {
+                                     options.PayloadSerializerSettings.ContractResolver = new DefaultContractResolver();
+                                 })
+                                 .WithAutomaticReconnect()
+                                 .Build();
+
+            hubConnection.StartAsync().GetAwaiter().GetResult();
+
+            Task.Run(async () =>
+            {
+                var channel = Channel.CreateBounded<string>(10);
+                await hubConnection.SendAsync("TestUpstream", channel.Reader);
+                await channel.Writer.WriteAsync("some data");
+                await channel.Writer.WriteAsync("some more data");
+                channel.Writer.Complete();
+            }).Wait();
+
+            Console.ReadKey();
+
+            hubConnection.DisposeAsync().GetAwaiter().GetResult();
+        }
+    }
+}
